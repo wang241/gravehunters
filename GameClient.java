d5326c20aa7d1c9ead0fe074019ebba4763c2eb7
Elizabeth Wang <wang241@mail.chapman.edu>
2017-04-06 10:00:15 -0700

    Added ClientListener.java, edited GameClient.java

---
+++ GameClient.java
@@ -0,0 +1,50 @@
+//GameClient.java
+//Adapted from https://github.com/ChapmanCPSC353/mtchat/
+
+import java.net.Socket;
+import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class GameClient.java
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			String hostname = "localhost";
+			int port = 7654;
+
+			System.out.println("Connecting to server on port " + port);
+			Socket connectionSock = new Socket(hostname, port);
+
+			DataOutputStream serverOutput = new DataOutputStream(connectionSock.getOutputStream());
+
+			System.out.println("Connection made.");
+
+			// Start a thread to listen and display data sent by the server
+			ClientListener listener = new ClientListener(connectionSock);
+			Thread theThread = new Thread(listener);
+			theThread.start();
+
+			// Read input from the keyboard and send it to everyone else.
+			Scanner keyboard = new Scanner(System.in);
+			while (true)
+			{
+				String data = keyboard.nextLine();
+				if(data.toLowerCase().indexOf("QUIT"))//Quit code
+				{
+					serverOutput.writeBytes("Connection Terminated by Client.");
+					break;
+				}
+				serverOutput.writeBytes(data + "\n");
+			}
+		}
+		catch (IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+} //GameClient

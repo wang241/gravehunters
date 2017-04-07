e6a3e10c06190fa2de740eb083560fd3b02fa7e6
chong127 <chong127@mail.chapman.edu>
2017-04-06 23:46:55 -0700

    Update GameBoard.java

--- GameBoard.java
+++ GameBoard.java
@@ -11,16 +11,17 @@ public class GameBoard
 {
 	public char[][] field;
 	private int len; //size of the grid
-	
+
 	GameBoard() //Generates a board
 	{
 		len = 6;
-		
+
 		//Random Number Generator
 		//int randomNum = ThreadLocalRandom.current().nextInt(0, 5 + 1);
 		//Put a method that checks "validSpace()", where True is when the space is a '-'
-		field = new char[len][len];
-		
+
+		field = new char[len][len]; //6x6 character array
+
 		for(int i = 0; i < len; i++)
 		{
 			for(int j = 0; j < len; j++)
@@ -40,12 +41,12 @@ public class GameBoard
 	GameBoard(int length)
 	{
 		len = length;
-		
+
 		//Random Number Generator
 		//int randomNum = ThreadLocalRandom.current().nextInt(0, 5 + 1);
 		//Put a method that checks "validSpace()", where True is when the space is a '-'
 		field = new char[len][len];
-		
+
 		for(int i = 0; i < len; i++)
 		{
 			for(int j = 0; j < len; j++)
@@ -61,12 +62,12 @@ public class GameBoard
 				}
 			}
 		}
-		
+
 	}
 	public String getBoard()
 	{
 		String temp = "";
-		
+
 		for(int i = 0; i < len; i++)
 		{
 			for(int j = 0; j < len; j++)
@@ -75,27 +76,34 @@ public class GameBoard
 			}
 			temp += '\n';
 		}
-		
+
 		return temp;
 	}
 	public void addPiece(int x, int y)
 	{
 		//Replaces '-' with another thing.
+		field[x][y] = 'O';
 	}
 	public void remPiece(int x, int y)
 	{
 		//Replaces spaces with '-'
+		field[x][y] = '-';
 	}
 	public boolean checkWin()
 	{
 		//Checks if all points have been collected from board
 		return true;
 	}
-	
+
 	//This is a checker method
 	public static void main(String[] args)
 	{
+
+		//addPiece(1,1);
 		GameBoard board = new GameBoard();
-		System.out.println(board.getBoard());		
+		board.addPiece(1,1);
+		System.out.println(board.getBoard());
+		board.remPiece(1,1);
+		System.out.println(board.getBoard());
 	}
 }//GameBoard
